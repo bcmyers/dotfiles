d3ca758a477016ac77ab0d7e14b2cf562a83268a
Brian Myers <brian.carl.myers@gmail.com>
2021-05-31 21:24:52 -0400

    Update backup-pass

--- scripts/bin/backup-pass.sh
+++ scripts/bin/backup-pass.sh
@@ -5,9 +5,15 @@ set -eux
 now=$(date -u +"%Y%m%d%H%M%S%z")
 
 filename="pass-${now}.tar.gz"
+local_path="/tmp/$filename"
 
-tar -cz --exclude=".git*" -f "$filename" --strip-components 3 "$HOME/.password-store"
+(
+    cd $HOME
+    tar -cz --exclude=".git*" -f "$local_path" .password-store
+)
 
-aws --profile bcmyers s3 cp "$filename" "s3://bcmyers-pass/$filename"
+aws --profile bcmyers s3 cp "$local_path" "s3://bcmyers-pass/$filename"
+
+rm "$local_path"
 
 echo "success"
