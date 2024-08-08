7334c74bfcf74c72aabe7aebc163e82e95cf49b7
Brian Myers <brian.myers@datadoghq.com>
2024-08-08 16:29:34 -0400

    update

--- install.sh
+++ install.sh
@@ -7,7 +7,15 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y \
+  curl \
+  libssl-dev \
   neovim \
   stow
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+cargo install eza ripgrep
 
 echo "Hello world" > /tmp/foo.txt
