85340580e6a9a35e43f049d7430933327afdbc65
Brian Myers <brian.myers@datadoghq.com>
2024-08-08 16:02:15 -0400

    update

--- install.sh
+++ install.sh
@@ -2,4 +2,12 @@
 
 set -euo pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+  neovim \
+  stow
+
 echo "Hello world" > /tmp/foo.txt
