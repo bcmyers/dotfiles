c444a8172656e2eb3586e8893d01c529e1b6e3ed
Brian Myers <brian.myers@datadoghq.com>
2024-08-09 09:10:17 -0400

    update

--- install.sh
+++ install.sh
@@ -2,20 +2,54 @@
 
 set -euo pipefail
 
-export DEBIAN_FRONTEND=noninteractive
+NVIM_BRANCH=v0.10.1
 
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y \
+  build-essential \
   curl \
+  fzf \
+  gettext \
+  htop \
   libssl-dev \
-  neovim \
   stow
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 export PATH="$HOME/.cargo/bin:$PATH"
 
-cargo install eza ripgrep
+cargo install \
+  bat \
+  cargo-update \
+  eza \
+  fd-find \
+  just \
+  ripgrep
 
-echo "Hello world" > /tmp/foo.txt
+if ! command -v nvim &> /dev/null; then
+  (
+    mkdir -p ~/opt
+    cd ~/opt
+    git clone --branch $NVIM_BRANCH --depth 1 https://github.com/neovim/neovim.git
+    cd neovim
+    make \
+      CMAKE_BUILD_TYPE=Release \
+      CMAKE_INSTALL_PREFIX=$HOME/.local \
+      install
+  )
+fi
+
+if ! command -v prompt &> /dev/null; then
+  (
+    mkdir -p ~/lib
+    cd ~/lib
+    git clone https://github.com/bcmyers/prompt.git
+    cd prompt
+    cargo install --root ~/.local --path .
+  )
+fi
+
+stow -v --target ~ fish
+stow -v --target ~ nvim
